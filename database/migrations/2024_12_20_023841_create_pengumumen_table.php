2229ff9e31ce53181bd4ad57f95b0e5b62a04ed5
syahrul anwar <syahrulanwar83@gmail.com>
2024-12-20 11:26:32 +0800

    add Pengumuman resource with CRUD pages and migration

---
+++ database/migrations/2024_12_20_023841_create_pengumumen_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengumumen', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body')->nullable();
+            $table->string('image')->nullable();
+            $table->dateTime('tanggal')->default(now());
+            $table->boolean('publish')->default(true);
+            $table->string('link_gmap')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pengumumen');
+    }
+};
