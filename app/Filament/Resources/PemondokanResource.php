bb9b5933a348a26ba777b969c3acbb562ba28534
syahrul anwar <syahrulanwar83@gmail.com>
2024-12-26 20:50:23 +0800

    fix: remove duplicate title input in PemondokanResource form

--- app/Filament/Resources/PemondokanResource.php
+++ app/Filament/Resources/PemondokanResource.php
@@ -48,8 +48,7 @@ class PemondokanResource extends Resource
                                 Forms\Components\TextInput::make('title')
                                     ->label('Nama Pemondokan')
                                     ->required(),
-                                    Forms\Components\TextInput::make('title')
-                                    ->label('Nama Pemondokan'),
+
                                 Forms\Components\TextInput::make('alamat')
                                 ->label('Jalan'),
                                 Forms\Components\TextInput::make('nama_kampung'),
