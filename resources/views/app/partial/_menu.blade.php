84db9dd2a88cf30c4bb026a087cdab7a9a1b9b77
hardygraph <hardygraph@gmail.com>
2024-12-27 16:36:24 +0800

    fix: correct spelling of 'Pemondokan' in menu partial

--- resources/views/app/partial/_menu.blade.php
+++ resources/views/app/partial/_menu.blade.php
@@ -7,7 +7,7 @@
             data-feather-size="32" data-feather-color="blue-dark"
             data-feather-bg="blue-fade-light"></i><span>Beranda</span></a>
     <a href="{{ route('pemondokan') }}"><i data-feather="bookmark" data-feather-line="1" data-feather-size="32"
-            data-feather-color="brown-dark" data-feather-bg="brown-fade-light"></i><span>Pemodokan</span></a>
+            data-feather-color="brown-dark" data-feather-bg="brown-fade-light"></i><span>Pemondokan</span></a>
     <a href="{{ route('direktori') }}"><i data-feather="book-open" data-feather-line="1" data-feather-size="32"
             data-feather-color="dark-dark" data-feather-bg="gray-fade-light"></i><span>Direktori</span></a>
 </div>
