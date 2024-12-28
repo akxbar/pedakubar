7c4dfd3439a343d07f00fa0be93838f3a9314e63
syahrul anwar <syahrulanwar83@gmail.com>
2024-12-28 14:14:11 +0800

    feat: add delete action with confirmation to KategoriDirektoriResource table

--- app/Filament/Resources/KategoriDirektoriResource.php
+++ app/Filament/Resources/KategoriDirektoriResource.php
@@ -63,6 +63,8 @@ class KategoriDirektoriResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make()
+                ->requiresConfirmation()
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
